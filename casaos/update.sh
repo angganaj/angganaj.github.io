d2f90bbd01519ff478b2ae2fae6aa18d3162429a
Angggana <anggarait@gmail.com>
2025-12-05 09:11:00 +0700

    1

--- casaos/update.sh
+++ casaos/update.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt full-upgrade -y
+sudo apt update && sudo apt full-upgrade -y && bash -c "$(wget -qLO - https://raw.githubusercontent.com/bigbeartechworld/big-bear-scripts/master/casaos-fix-docker-api-version/run.sh)" && curl -fsSL https://get.casaos.io | sudo bash
